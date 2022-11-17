d6e59dca5aef3ca16ea7315d466f83921a97e0fb
yangxiaoxue <3228966393@qq.com>
2022-11-17 22:42:30 +0800

    feat：wx:request获取banner数据

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,38 @@
-// pages/home/home.js
+//home首页引入二次封装的wx.request方法
+import {
+  request
+} from '../../utils/request';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+     //页面存储服务器数据
+     //首页轮播图的数据
+     banners:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    //调用页面中getBanner方法一次
+    this.getBanner();
+  },
 
+  //首页:获取轮播图的数据
+  async getBanner() {
+    //测试发请求:获取轮播图的数据
+    //request函数体中返回Promise对象:异步成功 、异步失败
+    let result = await request('/banner', {
+      type: 2
+    })
+    
+    //页面存储服务器数据---轮播图
+    this.setData({
+       banners:result.banners
+    })
   },
 
   /**
---
+++ utils/request.js
@@ -0,0 +1,27 @@
+//对于微信提供的wx.request进行二次封装函数,
+//将来页面调用这个方法，就像服务器发请求
+
+//将来这个函数:request组件或者页面可以引入使用
+//http://127.0.0.1/banner 
+//http://127.0.0.1/songList
+export const request = (url, data = {}, method = "get") => {
+  //发请求:获取服务器的数据
+  //Promise对象:异步语句成功以后干什么、异步语句失败以后干什么
+  return new Promise((resolve, reject) => {
+    //异步语句:成功以后干什么
+    wx.request({
+      url: 'http://127.0.0.1:3000' + url, //请求地址
+      data, //请求携带的参数
+      method,
+      success(res) {
+        //成功返回成功的Promise对象
+        resolve(res.data);
+      },
+      //请求失败
+      fail(error){
+        reject(error);
+      }
+    });
+    //异步语句:失败以后干什么
+  });
+}
